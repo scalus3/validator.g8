adb7bab41e4c2fdd638dfd503ee8b1d7db9df954
fernweh0 <aleksey.khodakovskiy@gmail.com>
2025-11-20 19:52:37 +0100

    chore: cleaning imports, adding comments, fix of the test

--- $name;format="Camel"$.test.scala
+++ $name;format="Camel"$.test.scala
@@ -6,22 +6,19 @@ import scalus.Compiler.compile
 import scalus.builtin.ByteString.*
 import scalus.builtin.Data
 import scalus.builtin.Data.toData
-import scalus.builtin.{ByteString, Data}
 import scalus.ledger.api.v1.PubKeyHash
 import scalus.ledger.api.v3.*
 import scalus.ledger.api.v3.given
 import scalus.prelude.*
 import scalus.testkit.ScalusTest
-import scalus.uplc.*
-import scalus.uplc.eval.*
 
 import scala.language.implicitConversions
-import scala.math.Ordering.Implicits.*
 
-class $name;format="Camel"$Test extends FunSuite, ScalusTest {
+class $name;format="Camel"$Test extends AnyFunSuite with ScalusTest {
 
     test("$name;format="Camel"$ should work correctly") {
 
+        // building htlc-like script context
         val inputs = List(
           makePubKeyHashInput(hex"1234567890abcdef1234567890abcdef1234567890abcdef12345678", 0),
         )
@@ -29,9 +26,9 @@ class $name;format="Camel"$Test extends FunSuite, ScalusTest {
           txInfo = TxInfo(inputs = inputs, id = random[TxId]),
           scriptInfo = ScriptInfo.SpendingScript(txOutRef = inputs.head.outRef)
         )
-
+        // running spend validator
         val result = compile($name;format="Camel"$.validate).runScript(context)
-
+        // should fail, since spend logic isn't implemented
         assert(result.isSuccess)
     }
 }
